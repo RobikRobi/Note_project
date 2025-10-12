9a4d2e5102e9fc9fcf25190295e4539669a4984b
admin <denis.lamochenko@gmail.com>
2025-10-12 19:59:58 +0800

    feature auth_router

--- src/models/NoteModel.py
+++ src/models/NoteModel.py
@@ -1,19 +1,30 @@
 import datetime
-import typing
 
 from sqlalchemy import ForeignKey, DateTime
 from sqlalchemy.orm import  Mapped, mapped_column,relationship
 from sqlalchemy.sql import func
 
+
 from src.db import Base
 
-class Message(Base):
+class Note(Base):
     __tablename__ = "note_table"
 
-    id:Mapped[int] = mapped_column(primary_key=True)
-    title:Mapped[str] = mapped_column(nullable=True)
-    content:Mapped[str] = mapped_column(nullable=True)
-    created_at:Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=func.now())
-    updated_at:Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=func.now())
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user_table.id'))
+    title: Mapped[list["Tags"]] = relationship(secondary="notetags_table", back_populates="notes", uselist=True)
+    content: Mapped[str] = mapped_column(nullable=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=func.now())
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=func.now())
 
-    text:Mapped[str]
+class Tags(Base):
+    __tablename__ = "tags_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    notes: Mapped[list["Note"]] = relationship(secondary="notetags_table", back_populates="title", uselist=True)
+
+class NoteTags(Base):
+    __tablename__ = "notetags_table"
+
+    note_id: Mapped[int] = mapped_column(ForeignKey('note_table.id'), primary_key=True)
+    tagse_id: Mapped[int] = mapped_column(ForeignKey('tags_table.id'), primary_key=True)
--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -1,24 +1,15 @@
-import datetime
-import typing
-
-from sqlalchemy import DateTime, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.sql import func
-from sqlalchemy import ForeignKey
 
-if typing.TYPE_CHECKING:
-    from src.models.MarkModel import Mark
-    from src.models.GroupModel import Group
-    from src.models.LessonModel import Lesson
 
 from src.db import Base
-
+from src.models.NoteModel import Note
 
 class User(Base):
     __tablename__ = "user_table"
 
-    id:Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
 
-    login:Mapped[str] = mapped_column(unique=True)
-    password:Mapped[bytes]
-    name:Mapped[str]
+    login: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[bytes]
+    name: Mapped[str]
+    notes: Mapped[list["Note"]] = relationship(back_populates="user_id", uselist=True)
